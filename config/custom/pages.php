0638dcc6aa1d6d512e04daeb1f4a53751e00980f
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2022-12-28 20:40:47 -0500

    add makeADonation link from production

--- config/custom/pages.php
+++ config/custom/pages.php
@@ -3,4 +3,5 @@
 return [
     'features' => env('FEATURES_ROUTE', 'https://www.wrmd.org/features'),
     'getHelp' => env('GET_HELP_ROUTE', 'https://help.wrmd.org/'),
+    'makeADonation' => env('MAKE_DONATION_ROUTE', 'https://www.wrmd.org/donate')
 ];
