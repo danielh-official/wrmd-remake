a8f04a80971ac8140469a134564736a79fd33ba2
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2022-12-28 23:26:35 -0500

    add more pages

--- config/custom/pages.php
+++ config/custom/pages.php
@@ -5,5 +5,9 @@ return [
     'getHelp' => env('GET_HELP_ROUTE', 'https://help.wrmd.org/'),
     'makeADonation' => env('MAKE_DONATION_ROUTE', 'https://www.wrmd.org/donate'),
     'register' => env('REGISTER_ROUTE', 'https://www.wrmd.org/register'),
-    'forgotPassword' => env('FORGOT_PASSWORD_ROUTE', 'https://www.wrmd.org/password/reset')
+    'forgotPassword' => env('FORGOT_PASSWORD_ROUTE', 'https://www.wrmd.org/password/reset'),
+    'termsAndPolicies' => env('TERMS_AND_POLICIES_ROUTE', 'https://www.wrmd.org/about/terms-and-conditions'),
+    'about' => env('ABOUT_ROUTE', 'https://www.wrmd.org/about'),
+    'blog' => env("BLOG_ROUTE", 'https://blog.wrmd.org/'),
+    'contact' => env('CONTACT_ROUTE', 'https://www.wrmd.org/about/contact')
 ];
