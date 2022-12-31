de3fff43519ee900cdc3bfe8ee3a5dc3674d2b59
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2022-12-31 11:13:31 -0500

    Update app.ts

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,12 +1,14 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/inertia-vue3';
+import {InertiaProgress} from '@inertiajs/progress';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
 // @ts-ignore
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
+import "flowbite";
+import 'tw-elements';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -15,8 +17,8 @@ createInertiaApp({
     // @ts-ignore
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     // @ts-ignore
-    setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
+    setup({el, app, props, plugin}) {
+        return createApp({render: () => h(app, props)})
             .use(plugin)
             // @ts-ignore
             .use(ZiggyVue, Ziggy)
@@ -24,4 +26,5 @@ createInertiaApp({
     },
 });
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({color: '#4B5563'});
+
