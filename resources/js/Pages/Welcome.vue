782f987d985b6cc77731ef55e85561fb00968389
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2022-12-31 11:13:47 -0500

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -9,6 +9,10 @@ export default {
 <script setup lang="ts">
 import {Head, Link} from '@inertiajs/inertia-vue3';
 import {ConfigInterface, UserInterface} from "../interfaces";
+import PrimaryButton from "../Components/PrimaryButton.vue";
+import WelcomeInfo from "../Components/Welcome/WelcomeInfo.vue";
+import WelcomeGetStarted from "../Components/Welcome/WelcomeGetStarted.vue";
+import WelcomeBenefitsExplanation from "../Components/Welcome/WelcomeBenefitsExplanation.vue";
 
 defineProps({
     canLogin: Boolean,
@@ -16,7 +20,9 @@ defineProps({
     laravelVersion: String,
     phpVersion: String,
     config: {required: true, type: Object as () => ConfigInterface},
-    user: Object as () => UserInterface
+    user: Object as () => UserInterface,
+    demoPhoto: {required: true, type: String},
+    logo: {required: true, type: String}
 });
 </script>
 
@@ -25,11 +31,12 @@ defineProps({
     <Head title="Welcome"/>
 
     <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div v-if="canLogin" class="fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="user" :href="route('dashboard')"
-                  class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard
-            </Link>
+        class="flex flex-col w-9/12 justify-center min-h-screen my-4">
+        <WelcomeInfo :logo="logo" :donate-now-url="config.custom.pages.donateNow"
+                     :donate-monthly-url="config.custom.pages.donateMonthly"></WelcomeInfo>
+        <WelcomeGetStarted></WelcomeGetStarted>
+        <div class="mt-3">
+            <WelcomeBenefitsExplanation :feature-route="config.custom.pages.features"></WelcomeBenefitsExplanation>
         </div>
     </div>
 </template>
