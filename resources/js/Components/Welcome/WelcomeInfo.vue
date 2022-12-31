a2d3478aa040fd0513156854fc0fb692ab51ee63
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2022-12-31 11:13:41 -0500

    Create WelcomeInfo.vue

---
+++ resources/js/Components/Welcome/WelcomeInfo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-gray-200 p-10 flex flex-col sm:flex-row space-x-3">
+        <div class="sm:block sm:h-10 w-[7rem] sm:flex-auto hidden" v-html="logo"></div>
+        <div class="text-sm text-gray-600 flex-auto sm:order-1 order-2">Wildlife Rehabilitation MD (WRMD) is a not-for-profit program
+            serving
+            over 1,000 wildlife rehabilitation
+            organizations across 28 countries. It's free to use but not free for us to maintain. Fewer than 5% of
+            our users have donated, but if everyone seeing this gave just $20 we would be set for 2023. Can You
+            Help?
+        </div>
+        <div class="flex flex-col flex-initial space-y-3 basis-1/12 sm:order-2 order-1 mb-2 sm:mb-0 text-center">
+            <PrimaryButton class="justify-center" target="_blank" :href="donateMonthlyUrl" color="green">Give Monthly
+            </PrimaryButton>
+            <PrimaryButton class="justify-center" target="_blank" :href="donateNowUrl" color="green">Give Now
+            </PrimaryButton>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import PrimaryButton from "../PrimaryButton.vue";
+
+defineProps({
+    donateMonthlyUrl: {required: true, type: String},
+    donateNowUrl: {required: true, type: String},
+    logo: {required: true, type: String}
+});
+</script>
