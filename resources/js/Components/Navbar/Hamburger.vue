1334451c25f1a5700d145fe709100eca78cc5c5b
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-01-06 17:47:13 -0500

    Update Hamburger.vue

--- resources/js/Components/Navbar/Hamburger.vue
+++ resources/js/Components/Navbar/Hamburger.vue
@@ -18,6 +18,7 @@ export default defineComponent({
 
 <template>
     <button
+        id="navbar-hamburger"
         class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition"
         @click="setShowingNavigationDropdown">
         <svg
