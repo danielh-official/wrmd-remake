ec5afd02286664badddb5d25fc58b95e7627b0e7
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-01-06 17:47:28 -0500

    Update AppLayout.vue

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -32,11 +32,11 @@ defineProps({
         <Banner/>
 
         <div class="min-h-screen bg-gray-100">
-            <slot name="navbar" v-if="$slots.navbar"></slot>
-            <Navbar v-else :config="config" :logo="logo" :jetstream="jetstream" :user="user"></Navbar>
+            <slot id="navbar" name="navbar" v-if="$slots.navbar"></slot>
+            <Navbar id="navbar" v-else :config="config" :logo="logo" :jetstream="jetstream" :user="user"></Navbar>
 
             <!-- Page Heading -->
-            <header v-if="$slots.header" class="bg-white shadow">
+            <header id="header" v-if="$slots.header" class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     <slot name="header"/>
                 </div>
