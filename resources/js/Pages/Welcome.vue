a550da0c796390eb1c6f35f0afc8700b3d46468e
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-01-06 14:28:16 -0500

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,6 +13,7 @@ import PrimaryButton from "../Components/PrimaryButton.vue";
 import WelcomeInfo from "../Components/Welcome/WelcomeInfo.vue";
 import WelcomeGetStarted from "../Components/Welcome/WelcomeGetStarted.vue";
 import WelcomeBenefitsExplanation from "../Components/Welcome/WelcomeBenefitsExplanation.vue";
+import TestimonialCard from "../Components/Welcome/TestimonialCard.vue";
 
 defineProps({
     canLogin: Boolean,
@@ -32,11 +33,12 @@ defineProps({
 
     <div
         class="flex flex-col w-9/12 justify-center min-h-screen my-4">
-        <WelcomeInfo :logo="logo" :donate-now-url="config.custom.pages.donateNow"
+        <WelcomeInfo class="mb-3" :logo="logo" :donate-now-url="config.custom.pages.donateNow"
                      :donate-monthly-url="config.custom.pages.donateMonthly"></WelcomeInfo>
         <WelcomeGetStarted></WelcomeGetStarted>
-        <div class="mt-3">
+        <div class="mt-3 flex flex-col sm:flex-row sm:space-x-7 space-y-2 sm:space-y-0">
             <WelcomeBenefitsExplanation :feature-route="config.custom.pages.features"></WelcomeBenefitsExplanation>
+            <TestimonialCard :testimonials-route="config.custom.pages.testimonials"></TestimonialCard>
         </div>
     </div>
 </template>
